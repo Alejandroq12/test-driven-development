64fc7a2a0ccf48060a01678a911dba8aef8fe3db
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-12-14 11:04:17 -0600

    Chore: add linters and fix linter errors.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -12,43 +12,43 @@ RSpec.describe Solver do
       result = solver.factorial(0)
       expect(result).to eq(1)
     end
-  
+
     it 'Returns 1 when n is 1' do
       solver = Solver.new
       result = solver.factorial(1)
       expect(result).to eq(1)
     end
-  
+
     it 'Returns correct factorial when n is 7' do
       solver = Solver.new
       result = solver.factorial(7)
       expect(result).to eq(5040)
     end
-  
+
     it 'Returns correct factorial when n is 12' do
       solver = Solver.new
       result = solver.factorial(12)
-      expect(result).to eq(479001600)
+      expect(result).to eq(479_001_600)
     end
   end
 
   describe '#reverse' do
-  it 'Correctly reverse a string' do
-    solver = Solver.new
-    result = solver.reverse('hello')
-    expect(result).to eq('olleh')
-  end
+    it 'Correctly reverse a string' do
+      solver = Solver.new
+      result = solver.reverse('hello')
+      expect(result).to eq('olleh')
+    end
 
-  it 'Correctly reverse a sentence with spaces' do
-    solver = Solver.new
-    result = solver.reverse('A little boy is coding.')
-    expect(result).to eq('.gnidoc si yob elttil A')
-  end
+    it 'Correctly reverse a sentence with spaces' do
+      solver = Solver.new
+      result = solver.reverse('A little boy is coding.')
+      expect(result).to eq('.gnidoc si yob elttil A')
+    end
 
-  it 'Correctly reverse a sentence with symbols' do
-    solver = Solver.new
-    result = solver.reverse('My password is Su**per/Secret/*')
-    expect(result).to eq('*/terceS/rep**uS si drowssap yM')
+    it 'Correctly reverse a sentence with symbols' do
+      solver = Solver.new
+      result = solver.reverse('My password is Su**per/Secret/*')
+      expect(result).to eq('*/terceS/rep**uS si drowssap yM')
+    end
   end
-end
 end
